4bcf5c40b8de8eb3c0dd99cab2a616c87c4b69a5
Kera <136464251+thekera404@users.noreply.github.com>
2025-08-10 21:31:10 +0600

    Add files via upload

---
+++ contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Second{
+    // How to store data dynamically
+
+    string public studentName;
+    int public studentAge;
+    string public courseName;
+    bool public isStudent;
+
+    function setProfile(string memory name, int age, string memory course) public {
+        studentName = name;
+        studentAge = age;
+        courseName = course;
+        isStudent = true;
+    }
+}
